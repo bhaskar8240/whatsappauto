18debfcb4445b14745fefde73461881572913ef2
Bhaskar debnath <55627401+bhaskar8240@users.noreply.github.com>
2022-04-16 16:03:04 +0530

    Update cs.py

--- cs.py
+++ cs.py
@@ -3,15 +3,16 @@ from pythonping import ping
 import datetime
 import time
 import re
-import pandas as pd
 
+# Regex create for Down and Up nodes.
 Down_RE = r'Node down'
 Up_RE = r'Node up'
 
+
+#Open the Ip text file for Ping test.
 with open("ipaddress.txt") as f:
     device_ip = f.read().splitlines()
-
-
+#Run the ping command through Module Pythoping (Where the Up and Down 
 def ping_status(x):
     respon = ping(x, count=2, size=9)        
     if respon.packets_lost:
@@ -20,13 +21,16 @@ def ping_status(x):
         return(f"{x} Node up")
 
 def log_collect(output=None):
+#If The nodes  are  down then add into txt file = down_networkinfo.txt file. 
     for line in output.split("\n"):
         k = re.findall(Down_RE, line)
+#If Host are already insert into the file then pass the node details.      
         if k:
             data= open("down_networkinfo.txt")
             for serach in data:
                 if line in serach:
                     break
+#If the Host data  not insert in the txt file then insert .                
             else:
                 data= open("down_networkinfo.txt","a")
                 print(line,datetime.datetime.now(),file=data)
@@ -38,26 +42,23 @@ def log_collect(output=None):
             else:
                 data= open("up_networkinfo.txt","a")
                 print(line,datetime.datetime.now(),file=data)
-            
-
-
+#Counting the Down host 
 def down_host():
     with open ("down_networkinfo.txt")as f:
         read = len(f.readlines())
         return  read
+#Counting the Uphost        
 def up_host():
     with open ("up_networkinfo.txt")as f:
         read = len(f.readlines())
-        return  read   
-
-
+        return  read 
+#Main function Run all the other function    
 def main():
     for ip in device_ip:
         ips=ip
         ping_start = ping_status(ips)
         log = log_collect(output=ping_start)
         Down_node = down_host()
-    
         Up_node = up_host()
     print("Up host:"+ str(Up_node))
     print("Down host:"+ str(Down_node)) 
