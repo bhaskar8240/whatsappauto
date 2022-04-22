1b6daa0ccad7014a2795bc31362e961cdff06bf2
Bhaskar debnath <55627401+bhaskar8240@users.noreply.github.com>
2022-04-23 00:08:56 +0530

    Update cs.py

--- cs.py
+++ cs.py
@@ -1,109 +1,109 @@
+# Import the module 
 from pythonping import ping
+import pywhatkit
 import datetime
 import time
 import re
-# Regex create for Down and Up nodes.
+
+#create re for better match
 Down_RE = r'Node down'
 Up_RE = r'Node up'
 
-
-#Open the Ip text file for Ping test.
-with open("whatsappauto-main/ipaddress.txt") as f:
+#Open the ip list of the host.
+with open("ipaddress1.txt") as f:
     device_ip = f.read().splitlines()
-#Run the ping command through Module Pythoping (Where the Up and Down 
+
+#Run the ping test. 
 def ping_status(x):
-    respon = ping(x, count=3, size=32)        
+    respon = ping(x, count=5, size=32)        
     if respon.packets_lost >= 1.0:
-        print(x)
         return(f"{x} Node down")                
     else:
         return(f"{x} Node up")
-
+#Down host log collect.(No Duplicate entry)
 def down_log_collect(output=None):
     for line in output.split("\n"):
         k = re.findall(Down_RE, line)    
         if k:
-            data= open("whatsappauto-main/down_networkinfo.txt")
+            data= open("down_networkinfo.txt")
             for serach in data:
                 if line in serach:
-                    
                     break
             else:
-                data= open("whatsappauto-main/down_networkinfo.txt","a")
+                data= open("down_networkinfo.txt","a")
                 print(line,datetime.datetime.now(),file=data)
-                
-
+#Up host log entry collect.(No duplicte entry)
 def up_log_collect(output=None):
     for line in output.split("\n"):
         k = re.findall(Up_RE, line)   
         if k:
-            data= open("whatsappauto-main/networkinfo.txt")
+            data= open("networkinfo.txt")
             for serach in data:
                 if line in serach:
                     
                     break
             else:
-                data= open("whatsappauto-main/networkinfo.txt","a")
+                data= open("networkinfo.txt","a")
                 print(line,datetime.datetime.now(),file=data)
-                
-
-
+#If host not up from Up host log Send in whats app group and remove from up list.
 def remove_if_not_up(output=None):
-    down=  open("whatsappauto-main/networkinfo.txt","r+")
+   
+    down=  open("networkinfo.txt","r")
     down_data = down.readlines()
-    for line in output.split("\n"):
-        down.seek(0)
-        for i in down_data:
-            if not (line.startswith(line)):
-               # pywhatkit.sendwhatmsg_to_group_instantly(f"Lmj4HDmcvkk91oAXSKMoXx","{line}Host is down")
-                down.write(i)             
-        down.truncate()
-
-
+    change=  open("networkinfo.txt","w")
+    for line in down_data:
+        if line.find(output) != -1:
+            sms= output + "Host is down"
+            pywhatkit.sendwhatmsg_to_group_instantly(f"Lmj4HDmcvkk91oAXSKMoXx",{sms},20,tab_close=True)
+            pass
+        else:
+            change.write(line)
+            
+#If host up from Down host log send in whats app group and remove from down list.
 def remove_if_not_down(output=None):
-    up= open("whatsappauto-main/down_networkinfo.txt","r+")
+    up= open("down_networkinfo.txt","r")
     up_data = up.readlines()
-    for line in output.split("\n"):
-        up.seek(0)
-        for i in up_data:
-            if not (line.startswith(line)):
-               # pywhatkit.sendwhatmsg_to_group_instantly(f"Lmj4HDmcvkk91oAXSKMoXx","{line}Host is up now")
-                up.write(i)        
-        up.truncate()
-
+    change= open("down_networkinfo.txt","w")
+    for line in up_data:
+        if line.find(output) != -1:
+            sms= output + "Host is Up"
+            pywhatkit.sendwhatmsg_to_group_instantly(f"Lmj4HDmcvkk91oAXSKMoXx",{sms},20,tab_close=True)
+            pass
+        else:
+            change.write(line)
+#Count the Down host.
 def down_host():
-    with open ("whatsappauto-main\down_networkinfo.txt")as f:
+    with open ("down_networkinfo.txt")as f:
         read = len(f.readlines())
         return  read
-#Counting the Uphost        
+#Count the Uphost.        
 def up_host():
-    with open ("whatsappauto-main/networkinfo.txt")as f:
+    with open ("networkinfo.txt")as f:
         read = len(f.readlines())
         return  read 
 #Main function Run all the other function    
 def main():
     while True:
-
         for ip in device_ip:
             ips=ip
             ping_start = ping_status(ips)
             if  "Node up" in ping_start:
                 updata = ping_start
-                up_log = up_log_collect(output=ping_start)         
+                up_ip_info = updata.replace("Node up","")
+                up_log = up_log_collect(output=ping_start)
+                send1_log_up = remove_if_not_down(output=up_ip_info)                 
             else:
-                downdata = ping_start 
+                downdata = ping_start  
+                ip_info = downdata.replace("Node down","")
                 down_log = down_log_collect(output=ping_start)
-                send_log_down = remove_if_not_up(output=downdata)
-        
-            Down_node = down_host()
-            Up_node = up_host()
+                send_log_down = remove_if_not_up(output=ip_info)
+
+        Down_node = down_host()
+        Up_node = up_host()
         print("Up host:"+ str(Up_node))
         print("Down host:"+ str(Down_node))
-        
-        send1_log_up = remove_if_not_down(output=updata)
+
         time.sleep(4) 
-
-
 
 if __name__ == "__main__":
      main()
